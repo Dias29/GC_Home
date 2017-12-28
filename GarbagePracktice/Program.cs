f6f971cc111bd112105142c318d84909bb4b1f40
УринбасаровД <@A-305-10>
2017-12-28 17:03:41 +0600

    first

---
+++ GarbagePracktice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbagePracktice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temp temp = new Temp("I", 1);
+
+            Console.WriteLine("Current generation: {0}", GC.GetGeneration(temp));
+            Console.ReadKey();
+            GC.Collect();
+            Console.WriteLine("Generation changed!");
+            Console.WriteLine("Current generation: {0}", GC.GetGeneration(temp));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GarbagePracktice/Temp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbagePracktice
+{
+    public class Temp
+    {
+        public string Tmp { get; set; }
+        public int Tnp { get; set; }
+        public Temp(string tmp, int tnp)
+        {
+            Tmp = tmp;
+            Tnp = tnp;
+        }
+
+    }
+}
